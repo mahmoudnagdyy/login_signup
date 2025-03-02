e03bd83f935e4b8ef6d771c25a56d15bdf472cdb
Mahmoud Nagdy <mahmoudnagdy65@gmail.com>
2025-03-02 18:03:13 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
     const useremail = login_email.current.value;
     const userpassword = login_password.current.value;
 
-    axios.post('https://backend-jade-zeta.vercel.app/auth/login', { email: useremail, password: userpassword })
+    axios.post('https://backend-jade-zeta.vercel.app/login', { email: useremail, password: userpassword })
     .then((res) => {
 
       if(res.data.token){
@@ -80,7 +80,7 @@ function App() {
       return ;
     }
 
-    axios.post('https://backend-jade-zeta.vercel.app/auth/signup', { name: username, email: useremail, password: userpassword })
+    axios.post('https://backend-jade-zeta.vercel.app/signup', { name: username, email: useremail, password: userpassword })
     .then((res) => {
       setResponse(res.data.message)
       const x = window.setTimeout(() => {
