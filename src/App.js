d378f4587add4ac6d2a985a65df1d19bc662d279
Mahmoud Nagdy <mahmoudnagdy65@gmail.com>
2025-03-03 15:33:41 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { useNavigate } from 'react-router-dom';
 
 function App() {
 
-  const [signShow, setSignShow] = useState(true);
-  const [response, setResponse] = useState('');
-  const [token, setToken] = useState('');
+  const [signShow, setSignShow] = useState(true)
+  const [response, setResponse] = useState('')
+  const [token, setToken] = useState('')
 
   const nav = useNavigate()
 
